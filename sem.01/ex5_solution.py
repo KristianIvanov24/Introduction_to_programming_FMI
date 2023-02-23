c097c629082a3cf6f45b2beccaaa670de0631f71
KristianIvanov24 <105101729+KristianIvanov24@users.noreply.github.com>
2023-02-23 16:37:55 +0200

    Create ex5_solution.py

---
+++ sem.01/ex5_solution.py
@@ -0,0 +1,6 @@
+# Отново задължително казваме на едната променлива, че исакме да е от целочислен тип
+
+name = input("Please input your name: ")
+age = int(input("Please input your age: "))
+
+print(f"Hello, {name}! You will be 100 years old after {100-age} years.") #питонски начин за изписване на екрана
