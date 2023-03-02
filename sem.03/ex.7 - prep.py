b7e4610317fe38482c98e0a60143ae247a2ed05d
vrsekulova <vrsekulova@uni-sofia.bg>
2023-03-02 10:25:06 +0200

    tasks on 02.03.2023 and some solutions, ex.4 for hw

---
+++ sem.03/ex.7 - prep.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+gui = Tk(className='Python Examples - Window Color')
+gui.geometry("400x200")
+
+background_color = ''
+#####################
+# !!Попълнете своя скрипт в това поле!!
+
+
+
+
+
+
+####################
+gui.configure(bg=background_color)
+
+gui.mainloop()
