22cd4f9a854119159471de56f8c3bb52b0a237b1
KristianIvanov24 <105101729+KristianIvanov24@users.noreply.github.com>
2023-05-15 11:26:39 +0300

    Create game_board.py

---
+++ sem.18/solutions/Kami-sol/game_board.py
@@ -0,0 +1,6 @@
+def game_board(lst):
+  print(f"| {lst[0]} | {lst[1]} | {lst[2]} |")
+  print(" -----------")
+  print(f"| {lst[3]} | {lst[4]} | {lst[5]} |")
+  print(" -----------")
+  print(f"| {lst[6]} | {lst[7]} | {lst[8]} |")
